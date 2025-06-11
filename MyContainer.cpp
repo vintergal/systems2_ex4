ecd85f0b2db655105b967fbb6cda4e4daf2a3640
gal vinter <vintergal@gmail.com>
2025-06-11 18:18:33 +0300

    init

---
+++ MyContainer.cpp
@@ -0,0 +1,4 @@
+namespace merchav
+{
+    
+} // namespace merchav
---
+++ MyContainer.hpp
@@ -0,0 +1,28 @@
+namespace merchav
+{
+    template<typename T> 
+    class MyContainer
+    {
+    private:
+        /* data */
+    public:
+        MyContainer(/* args */);
+        ~MyContainer();
+        void add(T item);
+        
+    };
+    
+    template<typename T>
+    MyContainer<T>::MyContainer(/* args */)
+    {
+    }
+    
+    template<typename T>
+    MyContainer<T>::~MyContainer()
+    {
+    }
+
+    
+    
+    
+} 
